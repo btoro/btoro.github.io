2e61369c3c845fc178b40334fdd41e81c5f39abb
btoro <btoro93@gmail.com>
2019-05-10 16:03:51 -0400

    'first

---
+++ js/background.js
@@ -0,0 +1,9 @@
+function randombg() {
+  var random = Math.floor(Math.random() * 3) + 0;
+  var bigSize = [
+    "url('../img/background2.png')",
+    "url('../img/background.jpg')",
+    "url('../img/background3.jpg')"
+  ];
+  document.getElementById("bg-img").style.backgroundImage = bigSize[random];
+}
