780983b94d489ec8e2d17f26732d38971a84d097
btoro <btoro93@gmail.com>
2019-05-15 09:54:22 -0400

    website update

--- js/background.js
+++ js/background.js
@@ -1,7 +1,7 @@
 function randombg() {
   var random = Math.floor(Math.random() * 3) + 0;
   var bigSize = [
-    "url('../img/background2.png')",
+    "url('../img/background2.jpg')",
     "url('../img/background.jpg')",
     "url('../img/background3.jpg')"
   ];
